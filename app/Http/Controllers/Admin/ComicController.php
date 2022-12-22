5407c156bc9477324d95032d751d7e95d930a5ba
Prico3 <pricoco.andrea@gmail.com>
2022-12-22 17:43:29 +0100

    problem with edit

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -82,9 +82,11 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Comic $comics)
     {
-        //
+        $data = $request->all();
+        $comics->update($data);
+        return redirect()->route('comics.show', $comics->id);
     }
 
     /**
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -5,6 +5,40 @@
 
 @section('content')
     <div class="container">
-        <h2>Modifica il fumetto</h2>
+        <h2>Modifica {{ $comics->title }}</h2>
+        <form action="{{ route('comics.update', $comics->id) }}" method="POST">
+            @method('PUT')
+            @csrf
+            <div class="mb-2">
+                <label for="title">Titolo</label>
+                <input type="text" class="form-control" id="title" name="title" value="{{ $comics->title }}">
+            </div>
+            <div class="mb-2">
+                <label for="description">Descrizione</label>
+                <textarea type="text" class="form-control" id="description" name="description">{{ $comics->description }}</textarea>
+            </div>
+            <div class="mb-2">
+                <label for="thumb">Immagine</label>
+                <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comics->thumb }}">
+            </div>
+            <div class="mb-2">
+                <label for="price">Prezzo</label>
+                <input type="text" class="form-control" id="price" name="price" value="{{ $comics->price }}">
+            </div>
+            <div class="mb-2">
+                <label for="series">Serie</label>
+                <input type="text" class="form-control" id="series" name="series" value="{{ $comics->series }}">
+            </div>
+            <div class="mb-2">
+                <label for="sale_date">Data di vendita</label>
+                <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ $comics->sale_date }}">
+            </div>
+            <div class="mb-2">
+                <label for="type">Genere</label>
+                <input type="text" class="form-control" id="type" name="type" value="{{ $comics->type }}">
+            </div>
+
+            <button type="submit" class="btn btn-success">Salva</button>
+        </form>
     </div>
 @endsection
