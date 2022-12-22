de606d3e51d5e7e6ec97613581c429c03c1b4f2b
Prico3 <pricoco.andrea@gmail.com>
2022-12-22 16:41:13 +0100

    methods create and store implemented

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,9 +6,40 @@
     <section>
         <div class="container">
             <h2 class="text-center">Crea un nuovo fumetto</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, repudiandae adipisci. Facere esse placeat
-                ad, incidunt quasi vero quia in! Deserunt libero illum soluta ipsam voluptates, harum expedita sint
-                voluptatum.</p>
+            <form action="{{ route('comics.store') }}" method="POST">
+                @csrf
+                <div class="mb-2">
+                    <label for="title">Titolo</label>
+                    <input type="text" class="form-control" id="title" name="title">
+                </div>
+                <div class="mb-2">
+                    <label for="description">Descrizione</label>
+                    <textarea type="text" class="form-control" id="description" name="description"></textarea>
+                </div>
+                <div class="mb-2">
+                    <label for="thumb">Immagine</label>
+                    <input type="text" class="form-control" id="thumb" name="thumb">
+                </div>
+                <div class="mb-2">
+                    <label for="price">Prezzo</label>
+                    <input type="text" class="form-control" id="price" name="price">
+                </div>
+                <div class="mb-2">
+                    <label for="series">Serie</label>
+                    <input type="text" class="form-control" id="series" name="series">
+                </div>
+                <div class="mb-2">
+                    <label for="sale_date">Data di vendita</label>
+                    <input type="text" class="form-control" id="sale_date" name="sale_date">
+                </div>
+                <div class="mb-2">
+                    <label for="type">Genere</label>
+                    <input type="text" class="form-control" id="type" name="type">
+                </div>
+
+                <button type="submit" class="btn btn-success">Salva</button>
+
+            </form>
         </div>
     </section>
 @endsection
